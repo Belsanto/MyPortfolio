e405578af8d054d411cd6fb95b31f0eb53dd22d9
Santiago Velandia Gallo <santiago.velandiag@uqvirtual.edu.co>
2023-10-13 00:26:54 -0500

    change about me

--- src/pages/PersonalCardItem/PersonalCardItem.js
+++ src/pages/PersonalCardItem/PersonalCardItem.js
@@ -17,23 +17,23 @@ function PersonalCardItem() {
 				<aside className="card-window" />
 				<aside className="card-text">
 					<p>
-						Soy un apasionado de la tecnología, tecnico en programación de software y estudiante de
-						ingenieria de sistemas y computación, con experiencia en el desarrollo de software y
-						soporte técnico. Mi pasión, además de la tecnología, son los videojuegos, la naturaleza
-						y los animales.
+						Soy un apasionado de la tecnología y el desarrollo de videojuegos en Unity, tecnico en
+						programación de software y estudiante de ingenieria de sistemas y computación, con
+						experiencia en el desarrollo de software y soporte técnico.
 					</p>
 					<div>
 						<h3>
-							<b>My Skills</b>
+							<b>Mis Habilidades</b>
 						</h3>
 						<SkillsContainer />
 					</div>
 					<p>
-						Hoy por hoy me encuentro cursando los últimos dos semestres en ingeniería de sistemas y
-						computación en la jornada nocturna. Cuento también con una sólida formación en HTML,
-						CSS, PL/SQL y bases de datos SQL. Además, tengo conocimientos en métodos y técnicas de
-						pruebas y calidad de software así como familiaridad con el uso herramientas de
-						automatización de pruebas.
+						He trabajado en el desarrollo de software integral para la gestión de turnos y en
+						proyectos administrativos utilizando tecnologías como Java, PHP, y PL/SQL. También tengo
+						habilidades en resolución de problemas como en métodos y técnicas de pruebas y calidad
+						de software así como familiaridad con el uso herramientas de automatización de
+						pruebas.Hoy por hoy me encuentro cursando los últimos dos semestres en ingeniería de
+						sistemas y computación en la jornada nocturna.
 					</p>
 				</aside>
 				<aside className="card-window" />
@@ -48,10 +48,10 @@ function PersonalCardItem() {
 						<b>ACTUALMENTE BUSCO AMPLIAR MI CAMPO LABORAL</b>
 					</h3>
 					<p>
-						Estoy entusiasmado por seguir creciendo profesionalmente en el campo de la tecnología y
-						contribuir al desarrollo de soluciones innovadoras. Si buscas a alguien apasionado, con
-						habilidades técnicas y capacidad de adaptación, estaré encantado de ser considerado para
-						oportunidades en el sector.
+						Estoy emocionado por seguir creciendo en el campo de desarrollo de software y/o
+						videojuegos y contribuir con soluciones creativas e innovadoras. Si estás en busca de un
+						profesional apasionado y con habilidades técnicas en software o desarrollo en Unity, no
+						dudes en contactarme.
 					</p>
 					{/*<p>Si quieres ponerte en contacto conmigo, por favor completa este pequeño <a href='#'>formulario</a></p>*/}
 				</aside>
